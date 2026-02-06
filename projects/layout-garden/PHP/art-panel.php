14af51b3501dca6fb9d792a8d7a47eb8a2b878f6
willhooper <willhooper103@gmail.com>
2026-02-06 13:05:27 -0600

    Changed image in layout-garden index

--- projects/layout-garden/PHP/art-panel.php
+++ projects/layout-garden/PHP/art-panel.php
@@ -4,11 +4,11 @@
 		<div class="art-panel">
 			<h1 class="attention-voice">On View Now</h1>
 		<picture>
-			<img src="images/alley-night.png" alt="">
+			<img src="images/moon-lake.jpg" alt="">
 		</picture>
 
 		<div class="art-info">
-			<h2>Will Hooper, City Alley</h2>
+			<h2>Will Hooper, Full Moon</h2>
 			<p>summer, 2025</p>
 		</div>
 	</section>
