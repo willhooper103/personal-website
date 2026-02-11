ddf2e5fd685057bd39a2b79c0a0e329c506a593e
willhooper <willhooper103@gmail.com>
2026-02-11 14:18:13 -0600

    Created php files and resized images.

---
+++ projects/layout-garden/human-ancestors/footer.php
@@ -0,0 +1,19 @@
+<!-- STUFF -->
+</main>
+
+
+<footer class="site-footer">
+  <div class="inner-column">
+  	<nav>
+    		<a href="something"> Welcome</a>
+    		<a href="something"> About</a>
+    </nav>
+  </div>
+</footer>
+	
+			
+
+</body>
+	
+
+</html>
---
+++ projects/layout-garden/human-ancestors/index.php
@@ -0,0 +1,91 @@
+
+<?php include('header.php'); ?>
+<!-- STUFF -->
+<main class="page-content">
+	  
+		  <section class="welcome"> 
+
+		  	<div class='skull-2'>
+		    		<img src="images/skull2.png" alt="">		    	
+		    </div>
+
+		    <div class='skull-3'>
+		    		<img src="images/skull3.png" alt="">		    	
+		    </div>
+		    <div class="inner-column">
+		    	<div class="welcome-info">
+		      
+		      
+			        <iframe width="500" height="300" src="https://www.youtube.com/embed/ehV-MmuvVMU" title="Human Origins 101 | National Geographic" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+			        	
+			        </iframe>
+			    
+			      
+			      
+			      	<h1>This is a brief intro to human evolution.</h1>
+			    </div>
+			    
+		     
+		    </div>
+		  </section>
+  
+	  <section class="about">
+
+	  		<div class='skull-1'>
+		    		<img src="images/skull1.png" alt="">		    	
+		    </div>
+	  		
+		    <div class="inner-column">
+	
+		    	<ul class="thing-list">
+		      
+			      <li class="thing-one">
+			        <picture>
+			            <img src="images/skull1.png" alt="">
+			        </picture>
+			        
+			        <div class="text">
+			              <h2>Early Humans</h2>
+			              <p>Some info about early humans.</p>
+			              <a href="https://humanorigins.si.edu/evidence/human-fossils/species/sahelanthropus-tchadensis" target="__blank" class="read-more"><button>learn more</button></a>
+			      </li>
+
+		    
+      
+    
+			       <li class="thing-two">
+			            <picture>
+			                <img src="images/skull1.png" alt="">
+			            </picture>
+
+			             <div class="text">
+			                  <h2>Early Humans</h2>
+			              <p>Some info about early humans.</p>
+			              <a href="https://humanorigins.si.edu/evidence/human-fossils/species/sahelanthropus-tchadensis" target="__blank" class="read-more"><button>learn more</button></a>
+			            </div>
+			       </li>
+			        
+      
+      
+			        <li class="thing-three">
+				        <picture>
+				            <img src="images/skull1.png" alt="">
+				        </picture>
+				        
+				         <div class="text">
+				              <h2>Early Humans</h2>
+			              <p>Some info about early humans.</p>
+			              <a href="https://humanorigins.si.edu/evidence/human-fossils/species/sahelanthropus-tchadensis" target="__blank" class="read-more"><button>learn more</button></a>
+				         </div>
+				      </li>
+	      
+	    </div>
+  </section>
+  
+  
+  
+</main>
+
+
+<?php include('footer.php'); ?>
+<!-- STUFF -->
