14f662c9f913f99491c3cac38e5e9c01ab53ae61
willhooper <willhooper103@gmail.com>
2026-02-11 13:04:22 -0600

    Made php file and css for my site index.

---
+++ my-site-index.php
@@ -0,0 +1,106 @@
+<!doctype html>
+<html>
+	
+	<head>
+		<meta lang="en">
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>my-site-index</title>
+		<meta name='description' content='This is the challenge-1 index'>
+		<meta name='og:image' content='challenge-1/images/memory-map.png'>
+		<meta name='og:image' content='challenge-1/images/note-app.png'>
+		<meta name='og:image' content='challenge-1/images/wired-revised.png'>
+		<meta name='og:image' content='challenge-1/images/collage.png'>
+		<link rel='stylesheet' href='index.css'>
+		<link rel='stylesheet' href='reset.css'>
+		<link rel='stylesheet' href='header.css'>
+	</head>
+		
+	<body>
+		<header class='site-header'>
+			<div class='inner-column'>
+
+				<?php include('header.php'); ?>
+				
+
+			</div>
+		</header>
+
+		<main class='page-content'>
+
+			<section class='about'>
+				<div class='inner-column'>
+					<div class='about-info'>
+						<picture class='headshot'>
+							<img src="challenge-1/images/blk-square.png" alt="" width="300" height="350">
+						</picture>
+
+						<div class='about-text'>
+							<h2>Here you will find my work from <a href="https://perpetual.education" target="_blank">Perpetual.Education</h2></p>
+						</div>
+					</div>
+				</div>
+			</section>
+
+			<section class='projects'>
+				<div class='inner-column'>
+
+					<section class='gallery'>
+						<ul class='project-list'>
+							<li><a href="https://instagram.com/willhooperart">
+							      <svg height="200" width="200">
+							      <rect x="0" y="0" width='200' height='200'/>
+							   	  </a></li>
+							<li><a href="https://instagram.com/willhooperart">
+							      <svg height="200" width="200">
+							      <rect x="0" y="0" width='200' height='200'/>
+							      </a></li>
+							<li><a href="https://instagram.com/willhooperart">
+							      <svg height="200" width="200">
+							      <rect x="0" y="0" width='200' height='200'/>
+							      </a></li>
+						
+							<li><a href="https://instagram.com/willhooperart">
+							      <svg height="200" width="200">
+							      <rect x="0" y="0" width='200' height='200'/>
+							      </a></li>
+							<li><a href="https://instagram.com/willhooperart">
+							      <svg height="200" width="200">
+							      <rect x="0" y="0" width='200' height='200'/>
+							      </a></li>
+							<li><a href="https://instagram.com/willhooperart">
+							      <svg height="200" width="200">
+							      <rect x="0" y="0" width='200' height='200'/>
+							      </a></li>
+
+						</ul>
+					</section>
+				</div>
+			</section>
+		</main>
+
+		<footer class='site-footer'>
+			<div class='inner-column'>
+				<ul class='footer-menu'>
+					<li>willhooper103@gmail.com</li>
+					<li>linkedin.com/</li>
+					<li>bluesky</li>
+				</ul>
+				
+			</div>
+			
+		</footer>
+	<body>
+<html>	
+
+
+
+
+
+
+
+
+
+	</body>
+		
+</html>
