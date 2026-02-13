0ab6453d04542ca91595370efe56cd052e6852ee
willhooper <willhooper103@gmail.com>
2026-02-13 11:42:54 -0600

    General work on my site landing page

--- about.php
+++ about.php
@@ -1,7 +1,10 @@
 <div class='about-info'>
-	
-
-	<div class='text-box'>
-		<p>Hi, my name is Will and I live on the the north side of Chicago. I have a fine art background and am also a musician. I have been studying web design and development with Perpetual Education since March of last year. Here you will find my work from <a href="https://perpetual.education" target="_blank">Perpetual.Education</p></a>
+	<div class="welcome">
+		<h1>Web Design and Development</h1>
 	</div>
+</div>	
+
+	<!-- <div class='text-box'> -->
+		<!-- <p>Hi, my name is Will and I live on the the north side of Chicago. I have a fine art background and am also a musician. I have been studying web design and development with Perpetual Education since March of last year. Here you will find my work from <a href="https://perpetual.education" target="_blank">Perpetual.Education</p></a> -->
+	<!-- </div> -->
 </div>
