904bc799883a9b09687ff4d12e99aae649172ec1
willhooper <willhooper103@gmail.com>
2026-02-20 11:05:32 -0600

    Changed images to jpg in 30-day-challenge

--- projects/30-day-graphics/php/graphics.php
+++ projects/30-day-graphics/php/graphics.php
@@ -9,36 +9,36 @@
 
 	<section class="gallery">
 	<ul class="image-list">
-		<li class="image"><img src="images/Day-1.png" alt=" day-1" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-2.png" alt=" day-2" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-3.png" alt=" day-3" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-4.png" alt=" day-4" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-5.png" alt=" day-5" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-6.png" alt=" day-6" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-7.png" alt=" day-7" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-8.png" alt=" day-8" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-9.png" alt=" day-9" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-10.png" alt=" day-10" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-12.png" alt=" day-11" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-13.png" alt=" day-12" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-14.png" alt=" day-13" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-15.png" alt=" day-14" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-16.png" alt=" day-15" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-17.png" alt=" day-16" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-17.png" alt=" day-17" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-18.png" alt=" day-18" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-19.png" alt=" day-19" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-20.png" alt=" day-20" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-21.png" alt=" day-1" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-22.png" alt=" day-1" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-23.png" alt=" day-1" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-24.png" alt=" day-1" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-25.png" alt=" day-1" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-26.png" alt=" day-1" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-27.png" alt=" day-1" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-28.png" alt=" day-1" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-29.png" alt=" day-1" width="300" height="300"></a></li>
-		<li class="image"><img src="images/Day-30.png" alt=" day-1" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-1.jpg" alt=" day-1" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-2.jpg" alt=" day-2" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-3.jpg" alt=" day-3" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-4.jpg" alt=" day-4" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-5.jpg" alt=" day-5" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-6.jpg" alt=" day-6" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-7.jpg" alt=" day-7" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-8.jpg" alt=" day-8" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-9.jpg" alt=" day-9" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-10.jpg" alt=" day-10" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-12.jpg" alt=" day-11" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-13.jpg" alt=" day-12" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-14.jpg" alt=" day-13" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-15.jpg" alt=" day-14" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-16.jpg" alt=" day-15" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-17.jpg" alt=" day-16" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-17.jpg" alt=" day-17" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-18.jpg" alt=" day-18" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-19.jpg" alt=" day-19" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-20.jpg" alt=" day-20" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-21.jpg" alt=" day-1" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-22.jpg" alt=" day-1" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-23.jpg" alt=" day-1" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-24.jpg" alt=" day-1" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-25.jpg" alt=" day-1" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-26.jpg" alt=" day-1" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-27.jpg" alt=" day-1" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-28.jpg" alt=" day-1" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-29.jpg" alt=" day-1" width="300" height="300"></a></li>
+		<li class="image"><img src="images/Day-30.jpg" alt=" day-1" width="300" height="300"></a></li>
 	</ul>	
 </div>
 	
