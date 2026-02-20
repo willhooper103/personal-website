ea0dce2dda45126b69d9af3200551d8599d6066b
willhooper <willhooper103@gmail.com>
2026-02-20 11:01:40 -0600

    Changed links in projects-module for my-site index.

--- php/projects-module.php
+++ php/projects-module.php
@@ -4,8 +4,8 @@
 	<h2>Projects</h2>
 
 	<ul>
-		<li><a href="projects/layout-garden">Layout Garden</a></li>
-		<li><a href="">30 Day Graphics</a></li>
+		<li><a href="projects/layout-garden/index.php">Layout Garden</a></li>
+		<li><a href="projects/30-day-graphics/index.php">30 Day Graphics</a></li>
 		<li><a href="">One Other</a></li>
 	</ul>
 	
