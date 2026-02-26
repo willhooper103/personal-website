e711e5682350b55da659792da3b445467d32a501
willhooper <willhooper103@gmail.com>
2026-02-26 09:44:29 -0600

    Created new layout-garden module.

---
+++ projects/layout-garden/rolling-stone.php
@@ -0,0 +1,46 @@
+
+
+<div class='rolling-stone-module'>
+	
+	<div class='column-1'>
+		<div class='column-1-heading'>
+			<h2>The Latest</h2>
+			
+		</div>
+		
+
+		<div class='text-box'>
+			<ul>
+				<li><h3>Large Text</h3>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, esse. Accusantium, sint beatae provident ullam at tempora laudantium dolores eos facilis minima, dolorum eum ea quas doloribus exercitationem, dolorem repellat!</p></li>
+				
+				<li><h3>Large Text</h3>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, esse. Accusantium, sint beatae provident ullam at tempora laudantium dolores eos facilis minima, dolorum eum ea quas doloribus exercitationem, dolorem repellat!</p></li>
+				
+				<li><h3>Large Text</h3>
+				<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore, esse. Accusantium, sint beatae provident ullam at tempora laudantium dolores eos facilis minima, dolorum eum ea quas doloribus exercitationem, dolorem repellat!</p></li>
+			</ul>
+			
+		</div>
+		
+	</div>
+
+	<div class='column-2'>
+
+		<picture>
+			<img src="images/news-panel.jpg" alt="">
+		</picture>
+
+		<h2>Larger Text</h2>
+		<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi necessitatibus consequatur cupiditate autem. Unde eum possimus libero corporis porro sapiente.</p>
+	</div>
+
+	<div class='column-3'>
+		<h1>VERY LARGE TEXT</h1>
+		<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis, maxime.</p>
+		
+	</div>
+	
+</div>
+
+
