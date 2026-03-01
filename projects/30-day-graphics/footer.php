f249582104af4dc86b2a63aabd62e20bf5455892
willhooper <willhooper103@gmail.com>
2026-03-01 09:31:10 -0600

    Worked on files.

---
+++ projects/30-day-graphics/footer.php
@@ -0,0 +1,28 @@
+
+<div class='footer-module'>
+
+	<div class='contact'>
+
+		<h2>Contact:</h2>
+
+		<ul>
+			<li>name</li>
+			<li>address</li>
+			<li>telephone</li>
+			<li>email</li>
+		</ul>
+	</div>
+
+	<div class='social'>
+
+		<h2>Find me here:</h2>
+
+		<ul>
+			<li><a href="https://bsky.app/profile/willhooperart.bsky.social">@willhooperart.bsky.social</a></li>
+			<li><a href="https://www.instagram.com/willhooperart">instagram.com/willhooperart</a></li>
+		</ul>
+		
+		
+	</div>
+	
+</div>
