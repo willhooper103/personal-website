2b5fb830758494f3aaa7a955650cacb9c7fd023c
willhooper <willhooper103@gmail.com>
2026-03-02 15:05:08 -0600

    Added graphic-panel-module to layout-garden.

---
+++ projects/layout-garden/graphic-panel-module.php
@@ -0,0 +1,62 @@
+<section class='graphic-panels'>
+
+  <div class="panel-1">
+    <picture>
+      <img src="images/news-panel.jpg" alt="" width='400'>
+
+    </picture>
+
+    <div class="info">
+      <h2 class='attention-voice'>PANEL-1</h2>
+      <p>This is a panel that shifts from horizontal to vertical, or, from row to column.</p>
+
+      <div class="action">
+        <a class="button pill" href="#">learn more</a>
+      </div>
+    </div>
+  </div>
+</section>
+
+<section class="graphic-panels">
+ 
+
+    <div class="panel-2 flipped">
+      <picture>
+        <img src="images/news-panel.jpg" alt="" width='400'>
+
+      </picture>
+
+      <div class="info">
+        <h2 class='attention-voice'>PANEL-2</h2>
+        <p>This is a panel that shifts from horizontal to vertical, or, from row to column.</p>
+
+        <div class="action">
+          <a class="button pill" href="#">learn more</a>
+        </div>
+      </div>
+    </div>
+</section>
+
+<section class='graphic-panels'>
+
+    <div class="panel-3">
+      <picture>
+        <img src="images/news-panel.jpg" alt="" width='400'>
+
+      </picture>
+
+      <div class="info">
+        <h2 class='attention-voice'>PANEL-3</h2>
+        <p>This is a panel that shifts from horizontal to vertical, or, from row to column.</p>
+
+        <div class="action">
+          <a class="button pill" href="#">learn more</a>
+        </div>
+      </div>
+    </div>
+</section>
+   
+
+
+
+</div>
