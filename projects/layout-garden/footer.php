ada61dbd9dd0a0dd5da9269d0082db6333c14073
willhooper <willhooper103@gmail.com>
2026-03-02 15:07:00 -0600

    Changed footer. Removed ‘social’

--- projects/layout-garden/footer.php
+++ projects/layout-garden/footer.php
@@ -1,25 +1,14 @@
-
 <div class='footer-module'>
 
-	<div class='contact'>
-
-		<h2>Contact:</h2>
-
-		<ul>
-			<li>name</li>
-			<li>address</li>
-			<li>telephone</li>
-			<li>email</li>
-		</ul>
-	</div>
-
-	<div class='social'>
+	<div id="contact" class="contact">
 
 		<h2>Find me here:</h2>
 
 		<ul>
-			<li><a href="https://bsky.app/profile/willhooperart.bsky.social">@willhooperart.bsky.social</a></li>
-			<li><a href="https://www.instagram.com/willhooperart">instagram.com/willhooperart</a></li>
+			<li>willhooper103@gmail.com</li>
+			<li> <a href="https://bsky.app/profile/willhooperart.bsky.social">@willhooperart.bsky.social</a></li>
+			<li> <a href="https://www.instagram.com/willhooperart">instagram.com/willhooperart</a></li>
+			<li> <a href="https://www.linkedin.com/in/william-hooper-9824ba312/">LinkedIn</a></li>
 		</ul>
 		
 		
