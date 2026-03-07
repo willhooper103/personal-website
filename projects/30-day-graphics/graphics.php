bae77728f46cc70b306379836faf36ab70019885
willhooper <willhooper103@gmail.com>
2026-03-06 20:45:47 -0600

    Made a container query for graphics module.

--- projects/30-day-graphics/graphics.php
+++ projects/30-day-graphics/graphics.php
@@ -1,45 +1,53 @@
-<div class='graphics-module'> 
 
-	   <section class="about">
+
+  	<section class="about">
 		  <p>These images are from a 30-day challenge from my course with Perpetual Education. Since I could do anything I wanted, I chose to narrow down my imagery and color palette for the sake of simplicity. By the end I was thinking of how I could refine the work and design for the web.</p>
-	</div>
-
 	</section>
 
+	<section>
 
-	<section class="gallery">
-		<ul class="image-list">
-			<li class="image"><img src="images/day-1.jpg" alt="day-1" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-2.jpg" alt="day-2" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-3.jpg" alt="day-3" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-4.jpg" alt="day-4" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-5.jpg" alt="day-5" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-6.jpg" alt="day-6" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-7.jpg" alt="day-7" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-8.jpg" alt="day-8" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-9.jpg" alt="day-9" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-10.jpg" alt="day-10" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-12.jpg" alt="day-11" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-13.jpg" alt="day-12" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-14.jpg" alt="day-13" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-15.jpg" alt="day-14" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-16.jpg" alt="day-15" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-17.jpg" alt="day-16" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-17.jpg" alt="day-17" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-18.jpg" alt="day-18" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-19.jpg" alt="day-19" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-20.jpg" alt="day-20" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-21.jpg" alt="day-21" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-22.jpg" alt="day-22" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-23.jpg" alt="day-23" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-24.jpg" alt="day-24" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-25.jpg" alt="day-25" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-26.jpg" alt="day-26" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-27.jpg" alt="day-27" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-28.jpg" alt="day-28" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-29.jpg" alt="day-29" width="300" height="300"></a></li>
-			<li class="image"><img src="images/day-30.jpg" alt="day-30" width="300" height="300"></a></li>
-	    </ul>	
-	 </section>
-</div>
+
+		<inner-column>
+			
+			<ol class='img-list'>	
+				<li data-repeat="30">
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-1.jpg" alt="day-1">
+							<img src="images/day-2.jpg" alt="day-2">
+							<img src="images/day-3.jpg" alt="day-3">
+							<img src="images/day-4.jpg" alt="day-4">
+							<img src="images/day-5.jpg" alt="day-5">
+							<img src="images/day-6.jpg" alt="day-6">
+							<img src="images/day-7.jpg" alt="day-7">
+							<img src="images/day-8.jpg" alt="day-8">
+							<img src="images/day-9.jpg" alt="day-9">
+							<img src="images/day-10.jpg" alt="day-10">
+							<img src="images/day-12.jpg" alt="day-11">
+							<img src="images/day-13.jpg" alt="day-12">
+							<img src="images/day-14.jpg" alt="day-13">
+							<img src="images/day-15.jpg" alt="day-14">
+							<img src="images/day-16.jpg" alt="day-15">
+							<img src="images/day-17.jpg" alt="day-16">
+							<img src="images/day-17.jpg" alt="day-17">
+							<img src="images/day-18.jpg" alt="day-18">
+							<img src="images/day-19.jpg" alt="day-19">
+							<img src="images/day-20.jpg" alt="day-20">
+							<img src="images/day-21.jpg" alt="day-21">
+							<img src="images/day-22.jpg" alt="day-22">
+							<img src="images/day-23.jpg" alt="day-23">
+							<img src="images/day-24.jpg" alt="day-24">
+							<img src="images/day-25.jpg" alt="day-25">
+							<img src="images/day-26.jpg" alt="day-26">
+							<img src="images/day-27.jpg" alt="day-27">
+							<img src="images/day-28.jpg" alt="day-28">
+							<img src="images/day-29.jpg" alt="day-29">
+							<img src="images/day-30.jpg" alt="day-30">
+						</picture>
+					</article>
+				</li>
+			</ol>
+		</inner-column>
+	</section>
+
 	
