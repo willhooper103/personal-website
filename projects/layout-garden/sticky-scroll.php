fc61fd3e795ada353d2454ca177394b1c0e87a0f
willhooper <willhooper103@gmail.com>
2026-03-06 20:38:14 -0600

    Created new layout-garden module.

---
+++ projects/layout-garden/sticky-scroll.php
@@ -0,0 +1,58 @@
+<section class='scroll-module'>
+	<div class='column-1'>
+		<h3>Header</h3>
+		<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Veniam, harum.</p>
+		<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Veniam, harum.</p>
+		<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Veniam, harum.</p>
+	</div>
+
+	<div class='column-2'>
+
+		<div class='column-2-info'>
+			<h3>Header</h3>
+			<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Veniam, harum.</p>
+		</div>
+
+		<div class='column-2-info'>
+			<h3>Header</h3>
+			<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Veniam, harum.</p>
+		</div>
+
+		<div class='column-2-info'>
+			<h3>Header</h3>
+			<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Veniam, harum.</p>
+		</div>
+
+		<div class='column-2-info'>
+			<h3>Header</h3>
+			<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Veniam, harum.</p>
+		</div>
+
+		<div class='column-2-info'>
+			<h3>Header</h3>
+			<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Veniam, harum.</p>
+		</div>
+
+		<div class='column-2-info'>
+			<h3>Header</h3>
+			<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Veniam, harum.</p>
+		</div>
+
+		<div class='column-2-info'>
+			<h3>Header</h3>
+			<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Veniam, harum.</p>
+		</div>
+
+		<div class='column-2-info'>
+			<h3>Header</h3>
+			<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Veniam, harum.</p>
+		</div>
+
+	</div>
+
+	<div class='column-3'>
+		<h3>Header</h3>
+		<p>Lorem, ipsum, dolor sit amet consectetur adipisicing elit. Veniam, harum.</p>
+	</div>
+	
+</section>
