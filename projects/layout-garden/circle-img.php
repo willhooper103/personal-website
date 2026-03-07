251bf8884b7be32ffa51288234d488175ea65cd3
willhooper <willhooper103@gmail.com>
2026-03-06 20:41:09 -0600

    Created new layout-garden module.

---
+++ projects/layout-garden/circle-img.php
@@ -0,0 +1,42 @@
+
+    <div class="circle-img-module">
+      
+      <div class="thing">
+        <picture>
+            <img src="images/orange-panel.png" alt="">
+        </picture>
+        
+        <div class="text">
+              <h2>Thing name here</h2>
+              <p>A little story to let you know what this is.</p>
+              <a href="#" class="read-more">read more</a>
+        </div>
+      </div>
+      
+    
+       <div class="thing">
+            <picture>
+                <img src="images/orange-panel.png" alt="#">
+            </picture>
+
+             <div class="text">
+                  <h2>Thing name here</h2>
+                  <p>A little story to let you know what this is.</p>
+                  <a href="" class="read-more">read more</a>
+            </div>
+        </div>
+      
+      
+        <div class="thing">
+          <picture>
+              <img src="images/orange-panel.png" alt="">
+          </picture>
+
+           <div class="text">
+                <h2>Thing name here</h2>
+                <p>A little story to let you know what this is.</p>
+                <a href="#" class="read-more">read more</a>
+          </div>
+        </div>
+      
+    </div>
