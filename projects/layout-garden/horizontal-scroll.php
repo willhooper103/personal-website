75993c64885126b964a831766ccc10ff662a5957
willhooper <willhooper103@gmail.com>
2026-03-24 10:13:06 -0500

    Created horizontal scroll module.

---
+++ projects/layout-garden/horizontal-scroll.php
@@ -0,0 +1,19 @@
+
+
+	<div class='scroll-container-wrapper'>
+		<button class='left-arrow' onclick='scrollLeft()'>&#9664</button>
+		<div class='horizontal-scroll-container' id='scroll-menu'>
+	
+			<div class='item'><img src="images/news-panel.jpg" alt=""></div>
+			<div class='item'><img src="images/news-panel.jpg" alt=""></div>
+			<div class='item'><img src="images/news-panel.jpg" alt=""></div>
+			<div class='item'><img src="images/news-panel.jpg" alt=""></div>
+			<div class='item'><img src="images/news-panel.jpg" alt=""></div>
+			<div class='item'><img src="images/news-panel.jpg" alt=""></div>
+			<div class='item'><img src="images/news-panel.jpg" alt=""></div>
+			</div>
+		<button class='right-arrow' onclick='scrollRight()'>&#9654</button>
+	</div>
+
+
+
