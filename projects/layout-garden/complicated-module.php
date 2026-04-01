9fa3265c74301461ddcc0101cd3e49f1adedfd1f
willhooper <willhooper103@gmail.com>
2026-04-01 14:17:48 -0500

    Created a new module.

---
+++ projects/layout-garden/complicated-module.php
@@ -0,0 +1,75 @@
+
+
+<div class='complicated-module'>
+
+	<left-section>
+
+		<div class='top-section'>
+
+			<picture>
+				<img src="images/news-panel.jpg" alt="">
+			</picture>
+
+			<picture>
+				<img src="images/news-panel.jpg" alt="">
+			</picture>
+
+		</div>
+
+		<div class='bottom-section'>
+
+			<div class='box-1'>
+				<p>This is a box with some info. It could could be an image or text.</p>
+			</div>
+
+			<div class='box-2'>
+				<p>This is a box with some info. It could could be an image or text.</p>
+			</div>
+
+			<div class='box-3'>
+				<p>This is a box with some info. It could could be an image or text.</p>
+			</div>
+	
+		</div>
+	</left-section>
+
+
+	<right-section>
+
+		<div class='top-section'>
+			<picture>
+				<img src="images/news-panel.jpg" alt="">
+			</picture>
+			
+		</div>
+
+		<div class='bottom-section'>
+
+			<div class='orange-column-1'>
+				<picture>
+					<img src="images/orange-panel.png" alt="">
+				</picture>
+
+				<picture>
+					<img src="images/orange-panel.png" alt="">
+				</picture>
+				
+			</div>
+
+			<div class='orange-column-2'>
+
+				<picture>
+					<img src="images/orange-panel.png" alt="">
+				</picture>
+
+				<picture>
+					<img src="images/orange-panel.png" alt="">
+				</picture>
+
+			</div>
+			
+		</div>
+		
+	</right-section>
+	
+</div>
