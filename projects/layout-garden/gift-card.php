54bb85abb5f722ddcd69f0cff26307d6f4d27d49
willhooper <willhooper103@gmail.com>
2026-04-18 14:21:44 -0500

    New layout-garden module.

---
+++ projects/layout-garden/gift-card.php
@@ -0,0 +1,87 @@
+
+
+<div class='gift-card'>
+	<picture>
+		<img src="images/news-panel.jpg" alt="">
+	</picture>
+
+	<div class='ordering-info'>
+		<h1>CREATE YOUR CARD</h1>
+
+		<h2>GIFT CARD DESIGN</h2>
+
+		<div class='amount'>
+			<h2>CHOOSE AN AMOUNT</h2>
+
+			<div class='dollar-amount'>
+				<ul>
+					<li>$25</li>
+					<li>$50</li>
+					<li>$100</li>
+					<li>$250</li>
+					<li>$500</li>
+				</ul>
+
+			</div>
+	
+			<p>Custom amount</p>
+		</div>
+
+		<div class='deliver-how'>
+			<h2>HOW WOULD YOU LIKE TO SEND IT?</h2>
+
+			<ul>
+				<li>Send by email</li>
+				<li>Get a link</li>
+			</ul>
+
+		</div>
+		
+		<div class='deliver-where'>
+			<h2>WHERE WOULD YOU LIKE TO SEND IT?</h2>
+
+			<ul>
+				<li>Recipient Name</li>
+
+				<div class='name-box'>
+					
+				</div>
+				<li>Recipient Email</li>
+
+				<div class='email-box'>
+				</div>
+			</ul>
+			
+		</div>
+	
+
+		<h2>ADD A MESSAGE(OPTIONAL)</h2>
+
+		<p>Your Message</p>
+		<div class='your-message'>
+			
+		</div>
+
+		<div class='divide-line'></div>
+
+		<h2>YOUR DETAILS</h2>
+
+		<p>Your Name</p>
+		<div class='your-name'></div>
+
+		<p>Your Email</p>
+		<div class='your-email'></div>
+
+		<h2>WHEN SHOULD IT BE DELIVERED?</h2>
+		<p>Delivery date</p>
+		<div class='delivery-date'></div>
+
+		<h2>QUANTITY</h2>
+		<div class='quantity'></div>
+
+		<button class='buy-now'>BUY NOW</button>
+		<button class='add-to-cart'>ADD TO CART</button>
+
+	</div>
+	
+</div>
