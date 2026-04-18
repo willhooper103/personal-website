3605df2cc296af848a2cee6e65e5a3edd9710e6c
willhooper <willhooper103@gmail.com>
2026-04-18 14:23:24 -0500

    Created new layout-garden module.

---
+++ projects/layout-garden/whitney.php
@@ -0,0 +1,49 @@
+
+
+	
+<whitney>
+	
+	<article class='on-view'>
+		<picture>
+			<img src="images/orange-panel.png" alt="">
+		</picture>
+		<h1>ON VIEW NOW</h1>
+	</article>
+
+	<article class='upcoming'>
+
+		<ul class='upcoming-list'>
+
+			<li>
+				<picture>
+					<img src="images/orange-panel.png" alt="">
+				</picture>
+				<h1>UPCOMING</h1>
+			</li>
+
+			<li>
+				<picture>
+					<img src="images/orange-panel.png" alt="">
+				</picture>
+				<h1>UPCOMING</h1>
+			</li>
+
+			<li>
+				<picture>
+					<img src="images/orange-panel.png" alt="">
+				</picture>
+				<h1>UPCOMING</h1>
+			</li>
+
+			<li>
+				<picture>
+					<img src="images/orange-panel.png" alt="">
+				</picture>
+				<h1>UPCOMING</h1>
+			</li>
+		</ul>
+
+	</article>
+	
+
+</whitney>
