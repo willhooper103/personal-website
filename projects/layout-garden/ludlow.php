85a302f193b62c04bf84d6b0b40a295b7366e0b8
willhooper <willhooper103@gmail.com>
2026-04-22 10:52:13 -0500

    Created new layout-garden module.

---
+++ projects/layout-garden/ludlow.php
@@ -0,0 +1,24 @@
+
+
+<ludlow>
+	<article>
+		<div class='bio'>
+			<h2>Here is a module where I am using css grid for the overall structure, and flexbox for certain items within that structure.</h2>
+	
+			<p>Something</p>
+
+			<p>Something else, like an address.</p>
+		</div>
+
+	</article>
+
+	<picture>
+		<source media='(300px <= width <= 600px)' srcset='images/portrait-panel.jpg'>
+
+		<source media='(600px <= width <= 900px)' srcset='images/news-panel.jpg'>
+
+		<source media='(min-width: 900px)' srcset='images/portrait-panel.jpg'>
+
+		<img src="images/portrait-panel.jpg" alt="">
+	</picture>
+</ludlow>
