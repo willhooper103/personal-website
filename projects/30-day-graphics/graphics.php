a1e9713940cf64fc456cdd96fe38432049b47566
willhooper <willhooper103@gmail.com>
2026-04-26 21:05:48 -0500

    Fixed errors in code to make containers query work.

--- projects/30-day-graphics/graphics.php
+++ projects/30-day-graphics/graphics.php
@@ -5,48 +5,127 @@
 	</section>
 
 	<section>
-
-
 		<inner-column>
 			
-			<ol class='img-list'>	
-				<li data-repeat="30">
+			<ol class='graphic-grid'>	
+				<li data-repeat='30'>
+
 					<article class='graphic-preview'>
 						<picture>
 							<img src="images/day-1.jpg" alt="day-1">
-							<img src="images/day-2.jpg" alt="day-2">
-							<img src="images/day-3.jpg" alt="day-3">
-							<img src="images/day-4.jpg" alt="day-4">
-							<img src="images/day-5.jpg" alt="day-5">
-							<img src="images/day-6.jpg" alt="day-6">
-							<img src="images/day-7.jpg" alt="day-7">
-							<img src="images/day-8.jpg" alt="day-8">
-							<img src="images/day-9.jpg" alt="day-9">
-							<img src="images/day-10.jpg" alt="day-10">
-							<img src="images/day-12.jpg" alt="day-11">
-							<img src="images/day-13.jpg" alt="day-12">
-							<img src="images/day-14.jpg" alt="day-13">
-							<img src="images/day-15.jpg" alt="day-14">
-							<img src="images/day-16.jpg" alt="day-15">
-							<img src="images/day-17.jpg" alt="day-16">
-							<img src="images/day-17.jpg" alt="day-17">
-							<img src="images/day-18.jpg" alt="day-18">
-							<img src="images/day-19.jpg" alt="day-19">
-							<img src="images/day-20.jpg" alt="day-20">
-							<img src="images/day-21.jpg" alt="day-21">
-							<img src="images/day-22.jpg" alt="day-22">
-							<img src="images/day-23.jpg" alt="day-23">
-							<img src="images/day-24.jpg" alt="day-24">
-							<img src="images/day-25.jpg" alt="day-25">
-							<img src="images/day-26.jpg" alt="day-26">
-							<img src="images/day-27.jpg" alt="day-27">
-							<img src="images/day-28.jpg" alt="day-28">
-							<img src="images/day-29.jpg" alt="day-29">
-							<img src="images/day-30.jpg" alt="day-30">
 						</picture>
-					</article>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-2.jpg" alt="day-2">
+						</picture>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-3.jpg" alt="day-3">
+						</picture>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-4.jpg" alt="day-4">
+						</picture>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-5.jpg" alt="day-5">
+						</picture>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-6.jpg" alt="day-6">
+						</picture>
+					</article>		
 				</li>
-			</ol>
+
+				<li data-repeat='30'>
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-1.jpg" alt="day-1">
+						</picture>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-2.jpg" alt="day-2">
+						</picture>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-3.jpg" alt="day-3">
+						</picture>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-4.jpg" alt="day-4">
+						</picture>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-5.jpg" alt="day-5">
+						</picture>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-6.jpg" alt="day-6">
+						</picture>
+					</article>		
+				</li>
+
+				<li data-repeat='30'>
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-1.jpg" alt="day-1">
+						</picture>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-2.jpg" alt="day-2">
+						</picture>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-3.jpg" alt="day-3">
+						</picture>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-4.jpg" alt="day-4">
+						</picture>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-5.jpg" alt="day-5">
+						</picture>
+					</article>	
+
+					<article class='graphic-preview'>
+						<picture>
+							<img src="images/day-6.jpg" alt="day-6">
+						</picture>
+					</article>		
+				</li>
+			</ol>		
+
 		</inner-column>
 	</section>
 
