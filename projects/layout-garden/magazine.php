42a1876ccaa6d2ba4c49c53fd7f9329a388dc596
willhooper <willhooper103@gmail.com>
2026-05-02 12:13:33 -0500

    Reconfigured magazine module structure.

--- projects/layout-garden/magazine.php
+++ projects/layout-garden/magazine.php
@@ -1,63 +1,59 @@
 
 
 <div class='magazine'>
-	
-		<div class='magazine-left'>
-			
-			<div class='green-box-top'>
 
-				<figure>
-					<video controls src="images/welcome-video.mov"></video>
+	<ul class='master-list'>
+		<ul class='left-items'>
+
+			<li><figure>
+				<video controls src="images/welcome-video.mov"></video>
 				</figure>
-
-			</div>
-
-			<div class='green-box-bottom'>
-
-				<picture>
-					<img src="images/portrait-panel.jpg" alt="">
-				</picture>
-		
-			</div>
+			</li>
 			
-		</div>
-
-		<div class='magazine-right'>
-
-			<div class='red-box-top'>
-				
-			</div>
-
-			<div class='red-box-middle'>
-
-				<div class='orange-box-left'>
-					<picture>
-						<img src="images/orange-panel.png" alt="">
-					</picture>
-				</div>
-
-				<div class='orange-group-right'>
-
-					<div class='orange-group-top'>
-						<picture>
-						<img src="images/orange-panel.png" alt="">
-					</picture>
-					</div>
-
-					<div class='orange-group-bottom'>
-						<picture>
-						<img src="images/orange-panel.png" alt="">
-					</picture>
-					</div>
+		</ul>
 					
-				</div>
-				
-			</div>
 
-			<div class='red-box-bottom'>
+		<ul class='right-items'>
+
+			<li>
+				<picture>
+					<img src="images/orange-panel.png" alt="">
+				</picture>
+			</li>
+
+			<li>
+				<picture>
+					<img src="images/orange-panel.png" alt="">
+				</picture>
+			</li>
+
+			<li>
+				<picture>
+					<img src="images/orange-panel.png" alt="">
+				</picture>
+			</li>
+
+			<li>
+				<picture>
+					<img src="images/orange-panel.png" alt="">
+				</picture>
+			</li>
+
+
+		</ul>
+
+		<div class='red-box-bottom'>
+			<p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perferendis, voluptatibus!</p>
 				
-			</div>
-			
 		</div>
-	
+
+    
+	</ul>
 </div>
+
+					
+			
+
+
+
+					
