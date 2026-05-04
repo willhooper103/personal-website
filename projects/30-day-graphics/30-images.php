a589bea68cfb9ab8220d349e911e3e39f084e360
willhooper <willhooper103@gmail.com>
2026-05-04 11:16:30 -0500

    Changed name of files to 30-images

---
+++ projects/30-day-graphics/30-images.php
@@ -0,0 +1,204 @@
+
+
+  	<section class="about">
+		  <p>These images are from a 30-day challenge from my course with Perpetual Education. Since I could do anything I wanted, I chose to narrow down my imagery and color palette for the sake of simplicity. By the end I was thinking of how I could refine the work and design for the web.</p>
+	</section>
+
+	<section>
+		<inner-column>
+			
+			<ol class='thirty-day-image-grid'>	
+				<li data-repeat='30'>
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-1.jpg" alt="day-1">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-2.jpg" alt="day-2">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-3.jpg" alt="day-3">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-4.jpg" alt="day-4">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-5.jpg" alt="day-5">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-6.jpg" alt="day-6">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-7.jpg" alt="day-7">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-8.jpg" alt="day-8">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-9.jpg" alt="day-9">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-10.jpg" alt="day-10">
+						</picture>
+					</article>		
+				</li>
+
+				<li data-repeat='30'>
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-11.jpg" alt="day-11">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-12.jpg" alt="day-12">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-13.jpg" alt="day-13">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-14.jpg" alt="day-14">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-15.jpg" alt="day-15">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-16.jpg" alt="day-16">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-17.jpg" alt="day-17">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-18.jpg" alt="day-18">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-19.jpg" alt="day-19">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-20.jpg" alt="day-20">
+						</picture>
+					</article>		
+				</li>
+
+				<li data-repeat='30'>
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-21.jpg" alt="day-21">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-22.jpg" alt="day-22">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-23.jpg" alt="day-23">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-24.jpg" alt="day-24">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-25.jpg" alt="day-25">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-26.jpg" alt="day-26">
+						</picture>
+					</article>	
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-27.jpg" alt="day-27">
+						</picture>
+					</article>
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-28.jpg" alt="day-28">
+						</picture>
+					</article>
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-29.jpg" alt="day-29">
+						</picture>
+					</article>
+
+					<article class='thirty-day-image-preview'>
+						<picture>
+							<img src="images/day-thirty.jpg" alt="day-thirty">
+						</picture>
+					</article>	
+				</li>
+			</ol>		
+
+		</inner-column>
+	</section>
+
+	
