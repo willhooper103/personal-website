389b7180f4417136bf9eae4edbdb07490b1c1098
willhooper <willhooper103@gmail.com>
2026-05-04 11:15:48 -0500

    Properly indented the code.

--- projects/30-day-graphics/index.php
+++ projects/30-day-graphics/index.php
@@ -1,57 +1,48 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>30-day-graphics</title>
-	<link rel="stylesheet" href="css/reset.css">
-	<link rel="stylesheet" href="css/index.css">
-	<link rel="stylesheet" href="css/graphics.css">
-	<link rel="stylesheet" href="css/header.css">
-	<link rel="stylesheet" href="css/footer.css">
-</head>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>30-images</title>
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/index.css">
+		<link rel="stylesheet" href="css/30-images.css">
+		<link rel="stylesheet" href="css/header.css">
+		<link rel="stylesheet" href="css/footer.css">
+	</head>
 
-<body>
-	
-	<section class="site-header">
-		<div class="inner-column">
-			
-			<?php include('header.php'); ?>
-		</div>
-			
-	</section>
-
-<main class="page-content">
-
-
-	<section class="section-one">
-		<div class='inner-column'>
-
-			 <?php include('graphics.php'); ?>
-					
-		</div> 
-	</section>
-
-
-	<section class="other-section">
-		<div class='inner-column'>
-
-			 <?php //include('big-banner.php'); ?>
-
-			 <?php //include('news-panel.php'); ?>
-					
-		</div> 
-	</section>
-</main>
-
-<footer class='site-footer'>
-	<div class="inner-column">
-		<?php include('footer.php'); ?>
+	<body>
 		
-	</div>
-	
-</footer>
+		<header class="site-header">
+			<div class="inner-column">
+				
+				<?php include('header.php'); ?>
+			</div>
+				
+		</header>
 
-</body>
+		<main class="page-content">
+
+			<section class="section-one">
+				<div class='inner-column'>
+
+				 	<?php include('30-images.php'); ?>
+						
+				</div> 
+
+			</section>
+
+		</main>
+
+		<footer class='site-footer'>
+			<div class="inner-column">
+
+				<?php include('footer.php'); ?>
+				
+			</div>
+			
+		</footer>
+
+	</body>
 </html>
 
--- projects/svg-30-day/index.php
+++ projects/svg-30-day/index.php
@@ -2,49 +2,47 @@
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>svg-30-day</title>
-	<link rel="stylesheet" href="css/reset.css">
-	<link rel="stylesheet" href="css/index.css">
-	<link rel="stylesheet" href="css/svg.css">
-	<link rel="stylesheet" href="css/header.css">
-	<link rel="stylesheet" href="css/footer.css">
-</head>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>svg-30-day</title>
+		<link rel="stylesheet" href="css/reset.css">
+		<link rel="stylesheet" href="css/index.css">
+		<link rel="stylesheet" href="css/svg.css">
+		<link rel="stylesheet" href="css/header.css">
+		<link rel="stylesheet" href="css/footer.css">
+	</head>
 
-<body>
-	
-	<section class="site-header">
-		<div class="inner-column">
-			
-			<?php include('header.php'); ?>
-		</div>
-			
-	</section>
-
-
-<main class="page-content">
-
-	<section class="section-one">
-		<div class='inner-column'>
-
-			 <?php include('svg.php'); ?>
-					
-		</div> 
-	</section>
-
-
-
-</main>
-
-<footer class='site-footer'>
-	<div class="inner-column">
-		<?php include('footer.php'); ?>
+	<body>
 		
-	</div>
-	
-</footer>
+		<header class="site-header">
+			<div class="inner-column">
+				
+				<?php include('header.php'); ?>
+			</div>
+				
+		</header>
 
-</body>
+
+		<main class="page-content">
+
+			<section class="section-one">
+				<div class='inner-column'>
+
+					 <?php include('svg.php'); ?>
+							
+				</div> 
+			</section>
+
+		</main>
+
+		<footer class='site-footer'>
+			<div class="inner-column">
+				<?php include('footer.php'); ?>
+				
+			</div>
+			
+		</footer>
+
+	</body>
 </html>
