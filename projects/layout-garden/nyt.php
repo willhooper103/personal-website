eb6a29aa8dd875823c8f02049ebb80ed3559e8b9
willhooper <willhooper103@gmail.com>
2026-05-26 20:16:43 -0500

    Created new layout-garden module.

---
+++ projects/layout-garden/nyt.php
@@ -0,0 +1,36 @@
+
+<nyt>
+
+	<article class='main-content'>
+		<h1>HEADING</h1>
+	</article>
+
+	<article class='news-stories'>
+		<ul class='stories-list'>
+
+			<li class='media-one'>
+				<h2>SUB-HEADING</h2>
+				<picture>
+					<img src="images/news-panel.jpg" alt="">
+				</picture>
+			</li>
+
+			<li class='media-two'>
+				<h2>SUB-HEADING</h2>
+				<picture>
+					<img src="images/news-panel.jpg" alt="">
+				</picture>
+			</li>
+
+			<li>
+				<p>Lorem, ipsum dolor sit amet, consectetur adipisicing elit. Repellendus possimus amet iusto dolores, fugiat corporis aspernatur magni dolore animi ipsam.</p>
+			</li>
+
+			<li>
+				<p>Lorem, ipsum dolor sit amet consectetur, adipisicing elit. Eaque eum quo voluptate, esse recusandae inventore, dicta. Sint facere doloremque fuga!</p>
+			</li>
+		</ul>
+
+	</article>
+
+</nyt>
