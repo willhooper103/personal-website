a43253a27072bfcf44800b17818be6698142ead0
willhooper <willhooper103@gmail.com>
2026-06-29 05:45:04 -0500

    Created section nineteen in index.

---
+++ projects/layout-garden/orangina.php
@@ -0,0 +1,26 @@
+
+<section-nineteen>
+	
+	<orangina>
+
+		<section class='main'>
+
+				<div class='orangina-img'></div>
+			
+			<section class='content'>
+				<h1>Orangina</h1>
+				<p>Favorite Thing</p>
+				<p>Other info</p>
+				<p>Other info</p>
+				
+			</section>
+
+			<section class='adoption'>
+				<h2>Adopted!</h2>
+				
+			</section>
+			
+		</section>
+
+</section-nineteen>
+
