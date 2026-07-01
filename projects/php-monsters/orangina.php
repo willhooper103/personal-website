2dc7a67665111d1647c8cdccbc7f4de9322bcb74
willhooper <willhooper103@gmail.com>
2026-07-01 09:42:57 -0500

    Created Orangina php to test styling.

---
+++ projects/php-monsters/orangina.php
@@ -0,0 +1,26 @@
+
+
+<orangina>
+
+		<section class='main'>
+
+				<picture>
+					<img src="images/orangina.jpg" alt="">
+				</picture>
+			
+			<section class='content'>
+				<h1>Orangina</h1>
+				<p>Favorite Thing</p>
+				<p>Other info</p>
+				<p>Other info</p>
+				
+			</section>
+
+			<section class='adoption'>
+				<h2>Adopted!</h2>
+				
+			</section>
+			
+		</section>
+		
+	</orangina>
