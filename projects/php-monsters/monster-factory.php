8494838ebdf8cdb5f1c2c5b48eef95728087e8a4
willhooper <willhooper103@gmail.com>
2026-08-03 11:40:51 -0500

    Added object array to php practice.

---
+++ projects/php-monsters/monster-factory.php
@@ -0,0 +1,92 @@
+
+<head>
+	<link rel="stylesheet" href="css/orangina copy.css">
+</head>
+
+<?php include('orangina.php'); ?>
+
+
+<?php
+
+
+$Cody = [
+	"id" => 1850,
+	"name" => "Cody",
+	"favoriteFood" => "Mountain Dew Code Red",
+	"age" => 37,
+	"adopted" => true,
+	"portrait" => "images/cody.jpg",
+];
+
+
+$Lima = [
+	"id" => 1851,
+	"name" => "Lima-bean",
+	"favoriteFood" => "mango",
+	"age" => 40,
+	"adopted" => false,
+	"portrait" => "images/lima-bean.jpg",
+];
+
+
+$Reads = [
+	"id" => 1852,
+	"name" => "Mrs. Reads-a-lot",
+	"favoriteFood" => "alphabet soup",
+	"age" => 38,
+	"adopted" => true,
+	"portrait" => "images/mrs-reads-a-lot.jpg",
+];
+
+
+$Orangina = [
+	"id" => 1853,
+	"name" => "Orangina",
+	"favoriteFood" => "Orange Fanta",
+	"age" => 40,
+	"adopted" => true,
+	"portrait" => "images/orangina.jpg",
+];
+
+
+$Shadow = [
+	"id" => 1854,
+	"name" => "Shadow",
+	"favoriteFood" => "eggplant",
+	"age" => 36,
+	"adopted" => true,
+	"portrait" => "images/shadow.jpg",
+	
+];
+
+
+$Fragoo = [
+	"id" => 1855,
+	"name" => "Fragoo",
+	"favoriteFood" => "pink lemonade",
+	"age" => 35,
+	"adopted" => true,
+	"portrait" => "images/fragoo.jpg",
+];
+
+
+$MrBanana = [
+	"id" => 1856,
+	"name" => "Mr. Banana",
+	"favoriteFood" => "bananas",
+	"age" => 41,
+	"adopted" => true,
+	"portrait" => "images/mr-banana.jpg",
+];
+
+
+// $story = $Cody["name"] . " is " . $Cody["age"] . " years old and his favorite food is " . $Cody["favoriteFood"] . ".";
+
+// $template = "<p id=" . $Cody["id"] . ">" . $story . "</p>";
+
+// echo $template;
+
+
+$monstersArray = [$Cody, $Fragoo, $Lima, $MrBanana, $Orangina, $Reads, $Shadow];
+
+?>
