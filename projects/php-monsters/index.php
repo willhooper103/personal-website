aa889787c856266480c126185c71a6f31f2bfef1
willhooper <willhooper103@gmail.com>
2026-08-03 11:42:38 -0500

    Removed object array from index.

--- projects/php-monsters/index.php
+++ projects/php-monsters/index.php
@@ -1,197 +1,179 @@
 
 
-<head>
-	<link rel="stylesheet" href="css/orangina.css">
-</head>
 
-<style>
-	ol {
-		display: grid;
-		list-style-type: none;
-	}
-
-	monster-card {
-		display: block;
-		outline: 1px solid black;
-		border-radius: 8px;
-		padding: 1rem;
-		background-color: hsl(100 50% 70%);
-	}
-
-	h2, p {
-		font-family: sans-serif;
-	}
-
-	img {
-		border-radius: 8px;
-	}
-
-	@media (width >= 500px) {
-		ol {
-			grid-template-columns: 1fr 1fr;
-		}
-	}
-</style>
-
-
-<?php
-
-$monsters = ["one", 1, "three", "cat" ];
-
-// echo "I have " . $monsters[0] . " " . $monsters[3] . ".";
-
-$Cody = [
-	"id" => 1850,
-	"name" => "Cody",
-	"favoriteFood" => "Mountain Dew Code Red",
-	"age" => 38,
-	"adopted" => true,
-	"portrait" => "images/cody.jpg",
-];
-
-
-$Lima = [
-	"id" => 1851,
-	"name" => "Lima-bean",
-	"favoriteFood" => "mango",
-	"age" => 40,
-	"adopted" => false,
-	"portrait" => "images/lima-bean.jpg",
-];
-
-
-$Reads = [
-	"id" => 1852,
-	"name" => "Mrs. Reads-a-lot",
-	"favoriteFood" => "Mountain Dew Code Red",
-	"age" => 38,
-	"adopted" => true,
-	"portrait" => "images/mrs-reads-a-lot.jpg",
-];
-
-
-$Orangina = [
-	"id" => 1853,
-	"name" => "Orangina",
-	"favoriteFood" => "Mountain Dew Code Red",
-	"age" => 38,
-	"adopted" => true,
-	"portrait" => "images/orangina.jpg",
-];
-
-
-$Shadow = [
-	"id" => 1854,
-	"name" => "Shadow",
-	"favoriteFood" => "Mountain Dew Code Red",
-	"age" => 38,
-	"adopted" => true,
-	"portrait" => "images/shadow.jpg",
-];
-
-
-$Fragoo = [
-	"id" => 1855,
-	"name" => "Fragoo",
-	"favoriteFood" => "Mountain Dew Code Red",
-	"age" => 38,
-	"adopted" => true,
-	"portrait" => "images/fragoo.jpg",
-];
-
-
-$MrBanana = [
-	"id" => 1856,
-	"name" => "Mr. Banana",
-	"favoriteFood" => "Mountain Dew Code Red",
-	"age" => 38,
-	"adopted" => true,
-	"portrait" => "images/mr-banana.jpg",
-];
-
-
-// $story = $Cody["name"] . " is " . $Cody["age"] . " years old and his favorite food is " . $Cody["favoriteFood"] . ".";
-
-// $template = "<p id=" . $Cody["id"] . ">" . $story . "</p>";
-
-// echo $template;
-
-
-
-
-$fruitsArray = ["apple", "banana", "peach", "mango", "strawberry"];
- 
-echo "<ol>";
-
-foreach ($fruitsArray as $fruit) {
-	// echo "<li>" . $fruit . "</li>";
-}
-
-// echo "</li>";
-
-echo "</ol>";
-
-
-
-
-
-$monstersArray = [$Cody, $Lima, $Reads, $MrBanana, $Fragoo, $Shadow, $Orangina];
-
-?>
-
-<ol>
-
+<header style='font-size: 1em'>
+	
 <?php 
 
-foreach ($monstersArray as $monster) {
-	$id = $monster["id"];
-	$name = $monster["name"];
-	$story = "My favorite food is " . $monster["favoriteFood"] . " and I am " . $monster["age"] . " years old";
-	$portrait = $monster["portrait"];
-	$status = $monster["adopted"];
-	$adopted = '';
+	function monsterGenerator($name, $age, $favoriteFood) {
+		$monster = [
+			"name" => $name,
+			"age" => $age,
+			"favoriteFood" => $favoriteFood,
+		];
+		return $monster;
 
-	// set human readable status message
+	 }
 
-	if ($status == 1) {
-		$status = "ADOPTED!";
-	} else {
-		$status = "NEEDS A HOME!";
-	}
+	$crunchy = monsterGenerator("Crunchy", 6, "pork rinds"); 
+	$crusty = monsterGenerator("Crusty", 6, "old bread");
+	$chunky = monsterGenerator("Chunky", 6, "corn on the cob");
+	$dusty = monsterGenerator("Dusty", 6, "pork rinds");
 
-	if ($monster["adopted"]) {
-		$adopted = 'adopted';
-	} 
+	$monsters = [$crunchy, $crusty, $chunky, $dusty];
 
 	?>
 
+	<?php foreach ($monsters as $m) { ?>
+		<li class='monster'>
+			<monster-card>
+				<h2 class='name'><?=$m["name"]?></h2>
+
+				<p class='age'><?=$m["age"]?></p>
+				<p class='food'><?=$m["favoriteFood"]?></p>
+			</monster-card>
+		</li>
+
+	<?php } ?>
 
 
-	<li class='monster <?=$adopted?> <?=$name?>'>;
 
-		<monster-card id='<?=$id?>'> 
 
-			<picture class='portrait'> 
-			<img src='<?=$portrait?>' width='300'> 
+	<?php 
+
+		function guitarlist($brand, $model, $color, $year) {
+			$guitar = [
+				"brand" => $brand,
+				"model" => $model,
+				"color" => $color,
+				"year" => $year,
+			];
+
+			return $guitar;
+		}
+
+		$fender = guitarlist("fender", "strat", "blonde", 1997);
+		$ibanez = guitarlist("ibanez", "artcore", "sunburst", 2004);
+		$alvarez = guitarlist("alvarez", "artist", "natural", 1996);
+
+		$guitars = [$fender, $ibanez, $alvarez];
+		
+	?>
+
+	<?php foreach ($guitars as $g) { ?>
+		<li class='guitar'>
+			<monster-card>
+				<h2 class='brand'><?=$g["brand"]?></h2>
+
+				<p class='model'><?=$g["model"]?></p>
+				<p class='color'><?=$g["color"]?></p>
+				<p class='year'><?=$g["year"]?></p>
+			</monster-card>
+		</li>
+
+	<?php } ?>
+
+	<?php
+
+	$sandwich = 6.99;
+	$tax = 0.10;
+	$total = $sandwich + ($sandwich*$tax);
+	$prettyTotal = number_format($total, 2, '.', ',');
+	echo "<p>Total: $$prettyTotal</p>";
+
+	 ?>
+
+
+
+	
+
+
+	<?php include('guitars_data.php'); ?>
+
+
+	<ol class='guitar-list'>
+		<?php foreach ($guitars as $guitar) { ?>
+			<guitar-card>
+				<h3><?php echo $guitar['name']; ?></h3>
+				<p><?=$guitar['brand']?></p>
+			</guitar-card>
+		<?php } ?>
+	</ol>
+
+ 
+</header>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<link rel="stylesheet" href="css/index.css">
+
+<?php include('monster-factory.php'); ?>
+<?php include('index-practice.php'); ?>
+
+
+<ol class='monster-list'>
+
+
+<?php foreach ($monstersArray as $monster) { ?>
+
+	<?php
+		$id = $monster["id"];
+		$name = $monster["name"];
+		$story = "My favorite food is " . $monster["favoriteFood"] . " and I am  " . $monster["age"] . ".";
+		$monster["name"];
+		$portrait = $monster["portrait"];
+		$status = $monster["adopted"];
+
+		if ($status ==1) {
+			$status = "Adpoted!";
+		} else {
+			$status = "Needs a home!";
+		}
+
+	?>
+
+	<li class='monster'>
+		<monster-card id='<?=$id?>'>
+			<picture class='portrait'>
+				<img src="<?=$portrait?>" alt="">
 			</picture>
+			<h2 class='name'><?=$name?></h2>
 
-			<h2 class='name'><?=$name?></h2> 
+			<p class='story'><?=$story?></p>
+			<p class='status'><?=$status?></p>
+			
+		</monster-card>
 
-			<p class='story'><?=$story?></p> 
-			<p class='status'><?=$status?></p> 
-
-		</monster-card>;
-
-	</li>;
+	</li>
 
 <?php } ?>
 
 
-
- </ol>
-
-<?php include('orangina.php'); ?>
-
+</ol> 
 
 
 
