5329e0376264a5b79f294f82e4a8fa1b3a0f4619
willhooper <willhooper103@gmail.com>
2026-08-03 11:39:55 -0500

    Added new object array to php practice.

---
+++ projects/php-monsters/guitars-data.php
@@ -0,0 +1,25 @@
+
+
+<?php
+
+   	$strat = [
+	 	"name" => "Stratocaster",
+	 	"brand" => "Fender",
+
+	 ];
+
+	 $lesPaul = [
+	 	"name" => "Les Paul",
+	 	"brand" => "Gibson",
+	 ];
+
+	 $super = [
+	 	"name" => "Super hard rocker v2",
+	 	"brand" => "Jackson",
+	 ];
+
+
+
+	 $guitars = [$strat, $lesPaul, $super];
+
+?>
