fe9d960162ec90af8a96ddbab6ab385263d612f0
willhooper <willhooper103@gmail.com>
2026-08-12 11:56:15 -0500

    Changed variables in guitars_data.php file.

--- projects/php-monsters/guitars_data.php
+++ projects/php-monsters/guitars_data.php
@@ -5,17 +5,23 @@
    	$strat = [
 	 	"name" => "Stratocaster",
 	 	"brand" => "Fender",
+	 	"price" => 1200.00,
+	 	"onsale" => true,
 
 	 ];
 
 	 $lesPaul = [
 	 	"name" => "Les Paul",
 	 	"brand" => "Gibson",
+	 	"price" => 799.00,
+	 	"onsale" => false,
 	 ];
 
 	 $super = [
 	 	"name" => "Super hard rocker v2",
 	 	"brand" => "Jackson",
+	 	"price" => 680.98,
+	 	"onsale" => true,
 	 ];
 
 
