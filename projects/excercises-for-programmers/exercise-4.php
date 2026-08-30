71397dbfe42ffb7146e38f48094e60ebef8ba324
willhooper <willhooper103@gmail.com>
2026-08-30 15:26:44 -0500

    Created exercise 4. Added ‘value’ to .field.

--- projects/excercises-for-programmers/exercise-4.php
+++ projects/excercises-for-programmers/exercise-4.php
@@ -1,49 +1,117 @@
 
 
 <style>
+
+	heading {
+		font-size: 2rem;
+	}
+
 	form {
-		max-width: 400px;
+		display: grid;
+		max-width: 300px;
+		gap: 16px;
 		outline: 1px solid black;
+		background-color: wheat;
 		border-radius: 8px;
 		padding: 1rem;
+	}
+
+	.field {
+		display: grid;
+		input[type='text'] {
+			padding: .3em;
+		}
+	}
+
+	label {
+		font-size: 1.2rem;
+	}
+
+	label + input {
+		margin-top: .5rem;
+	}
+
+	output {
+		font-size: 1.5rem;
 	}
 </style>
 
 
 <?php
+
+$noun = "";
+$verb = "";
+$adjective = "";
+$adverb = "";
+$madLib = "";
+$errorMessage = "";
+
+function madlibStatement($n, $v, $adj, $adv) {
+	return ("Do you $v your $adj $n $adv? That's hilarious!");
+}
+
+
 if (isset($_POST['submitted'])) {
-	if (isset($_POST['user_name']) && !empty($_POST['user_name'])) {
-		if ($_POST['user_name']) {
-			$name = $_POST['user_name'];
-		}
+	$noun = trim($_POST['noun'] ?? '');
+	$verb = trim($_POST['verb'] ?? '');
+	$adjective = trim($_POST['adjective'] ?? '');
+	$adverb = trim($_POST['adverb'] ?? '');
+
+
+	if (empty($noun)) {
+		$errorMessage = "please enter a noun.";
+	} elseif (empty($verb)) {
+		$errorMessage = "please enter a verb.";
+	} elseif (empty($adjective)) {
+		$errorMessage = "please enter an adjective.";
+	} elseif (empty($adverb)) {
+		$errorMessage = "please enter an adverb.";
+	} else {
+		$madLib = madlibStatement($noun, $verb, $adjective, $adverb);
 	}
 }
+
 ?>
 
 
 <form method='POST'>
-	<p>Exercise 4</p>
+	<heading><strong>Mad Lib</strong></heading>
 
-	<div clas='field'>
-		<label>What is exercise 4?</label>
-		<input type="text" name="user_name">
+	<?php if (!empty($errorMessage));?>
+	<p style="color: red;"><?=$errorMessage?></p>
+
+	<div class='field'>
+		<label>noun</label>
+		<input type="text" name='noun' value= '<?=$noun?>'>
 	</div>
 	
-	<button type="submit" name="submitted">Enter</button>
 
-	<div clas='field'>
-		<label>What is exercise 4?</label>
-		<input type="text" name="user_name">
+	<div class='field'>
+		<label>verb</label>
+		<input type='text' name='verb' value= '<?=$verb?>'>
+	</div>
+
+
+	<div class='field'>
+		<label>adjective</label>
+		<input type='text' name='adjective' value= '<?=$adjective?>'>
+	</div>
+
+
+	<div class='field'>
+		<label>adverb</label>
+		<input type='text' name='adverb' value= '<?=$adverb?>'>
 	</div>
 
 	<button type="submit" name="submitted">Enter</button>
 
-	<div clas='field'>
-		<label>What is exercise 4?</label>
-		<input type="text" name="user_name">
-	</div>
-
-	<button type="submit" name="submitted">Enter</button>
+	<output>
+		 <?=$madLib?>
+	</output>
 	
 	
 </form>
+
+
+
+
