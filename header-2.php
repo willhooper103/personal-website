80a90e5505b53fa089fef11f72742c3b63302e85
willhooper <willhooper103@gmail.com>
2026-09-02 12:28:12 -0500

    Created a new header for site landing page.

---
+++ header-2.php
@@ -0,0 +1,17 @@
+<div class='header-list'>
+
+	<span class='artist-name'><a href="#">Will Hooper</a></span>
+
+	<ul class='nav-menu'>
+
+		<li class="dropdown"><a href="#">work</a></li>
+
+		 	<ul class="dropdown-menu">
+				<li><a href="my-site/projects/layout-garden.php">layout-garden</a></li>
+				<li><a href="my-site/projects/30-day-graphics.php">30-day graphics</a></li>
+			</ul>
+
+		<li><a href="#about">about</a></li> 
+		<li><a href="#contact">contact</a></li> 
+	</ul>
+</div>
