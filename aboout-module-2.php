6078f16f62820521fa8d384250fc9d1ee6c76af8
willhooper <willhooper103@gmail.com>
2026-09-02 12:33:04 -0500

    Created  new php/css files for the landing page

---
+++ aboout-module-2.php
@@ -0,0 +1,10 @@
+
+
+<div id="about" class='about-module'>
+
+	<div class='about-text'>
+		<p>Web Designer / Developer with a fine art background</p>
+		
+	</div>
+	
+</div>
---
+++ about-module-2.php
@@ -0,0 +1,9 @@
+
+
+<div class='about-module'>
+
+	<div class='about-text'>
+		<p>Web designer / developer with a fine art background</p>
+		
+	</div>
+</div>
