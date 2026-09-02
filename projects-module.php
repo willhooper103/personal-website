6d9aba8b2c7042dae5dda49c6e05fbfaa412dd8f
willhooper <willhooper103@gmail.com>
2026-09-02 12:37:03 -0500

    Commented out an h2 on the landing page.

--- projects-module.php
+++ projects-module.php
@@ -1,13 +1,11 @@
 
 
 <div id="projects" class='projects-module'>
-	<h2>Projects</h2>
+	<!-- <h2>Projects</h2> -->
 
 	<ul>
 		<li><a href="projects/layout-garden/index.php">Layout Garden</a></li>
-		<li><a href="projects/30-day-graphics/index.php">30 Day Graphics</a></li>
-		<li><a href="projects/svg-30-day/index.php">SVG 30 Day Challenge</a></li>
-		
+		<li><a href="projects/30-day-graphics/index.php">30 Day Graphics</a></li>	
 	</ul>
 	
 </div>
