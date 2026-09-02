673b2718f937ca3c230600590e7c7173b3e594f9
willhooper <willhooper103@gmail.com>
2026-09-02 12:34:33 -0500

    Removed/added sections in the landing page index.

--- index.php
+++ index.php
@@ -13,59 +13,41 @@
 		<meta name='og:image' content='challenge-1/images/collage.png'>
 		<link rel='stylesheet' href='css/index.css'>
 		<link rel='stylesheet' href='css/reset.css'>
-		<link rel='stylesheet' href='css/header.css'>
+		<link rel='stylesheet' href='css/header-2.css'>
 		<link rel='stylesheet' href='css/footer.css'>
-		<link rel='stylesheet' href='css/welcome.css'>
-		<link rel='stylesheet' href='css/about-module.css'>
+		<link rel='stylesheet' href='css/welcome-2.css'>
+		<link rel='stylesheet' href='css/about-module-2.css'>
 		<link rel='stylesheet' href='css/projects-module.css'>
 	</head>
 		
 	<body>
 		<header class='site-header'>
-			<div class='inner-column'>
 
-				<?php include('header.php'); ?>
-				
+			<div class='inner-column'>
+				<?php include('header-2.php'); ?>
 			</div>
+
 		</header>
 
 		<main id="index" class='page-content'>
 
 			<section class='welcome'>
+
 				<div class='inner-column'>
-
-					<?php include('welcome.php'); ?>
-				
-					
-				</div>
-			</section>
-
-			<section class='about'>
-				<div class='inner-column'>
-					
-					<?php include('about-module.php'); ?>
-				</div>
-
+					<?php include('welcome-2.php'); ?>
+				</div>	
 				
 			</section>
 
-			<section class='projects'>
+			<section class='projects-module'>
+				
 				<div class='inner-column'>
-					
 					<?php include('projects-module.php'); ?>
-			
-				</div>			
-						
-			</section>
-		</main>
-
-		<footer class='site-footer'>
-			<div class='inner-column'>
+				</div>	
 				
-				<?php include('footer.php'); ?>
-			</div>
-			
-		</footer>
+			</section>
+
+	
 	</body>
 </html>	
 
